27066a4432fd6ee57807d02e8813c2a20c559e71
Ryan-Dwiyanto <ryandwiyanto362@gmail.com>
2025-11-18 11:43:28 +0700

    Update

---
+++ lib/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
